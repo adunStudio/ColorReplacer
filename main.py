617b2513baa9112041f589b2f9e0f38cb481121c
GodHongIl <adunstudio@daum.net>
2020-08-11 16:23:52 +0900

    개발 완료

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import os
+from PIL import Image
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+old_R, old_G, old_B = 21, 21, 21
+new_R, new_G, new_B = 51, 50, 61
+count = 0
 
+def PrintFilesInDir(rootDir, prefix):
+    files = os.listdir(rootDir)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    for file in files:
+        path = os.path.join(rootDir, file)
+        if os.path.isdir(path):
+            print(prefix + path)
+            PrintFilesInDir(path, prefix + "        ")
+        else:
+            global count
+            count = count + 1
+            print(prefix + file)
+            ext = os.path.splitext(file)[1]
+            if ext == ".png":
+                ReplaceColor(path)
 
+def ReplaceColor(path):
+    image = Image.open(path)
+    image = image.convert("RGBA")
+    data = np.array(image)
+    red, green, blue =  data[:,:,0], data[:,:,1], data[:,:,2]
+    mask = (red == old_R) & (green == old_G) & (blue == old_B)
+    data[:, :, :3][mask] = [new_R, new_G, new_B]
+    newImage = Image.fromarray(data)
+    newImage.save(path)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+rootDir = "./Resources"
+PrintFilesInDir(rootDir, "")
+print(count)
